edc8c961deaa09dcbbce17a89239c4312567fcf8
Rachael Abraham <rabraham_iloh@yahoo.com>
2026-05-21 20:02:32 -0500

    Fix homepage completely

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <main style={{
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "40px",
-      fontWeight: "bold"
-    }}>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
       Doing Things Right Lets Talk
     </main>
   );
